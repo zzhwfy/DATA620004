8bc9fb47a4a240396b4a6f31dc4660e8faa54116
zzhwfy <57525119+zzhwfy@users.noreply.github.com>
2022-05-12 16:22:42 +0800

    Add files via upload

---
+++ backbone/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.7.0"
+from .EfficeientNet import EfficientNet, VALID_MODELS
+from .utils import (
+    GlobalParams,
+    BlockArgs,
+    BlockDecoder,
+    efficientnet,
+    get_model_params,
+)
